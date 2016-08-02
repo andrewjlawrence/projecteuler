26d3fe17f3df4adc96cf8727ad71a7b84a32736a
andrewjlawrence <ajlawrence@acm.org>
2016-08-02 21:52:44 +0100

    added fast implementation

--- problem-1/main.cpp
+++ problem-1/main.cpp
@@ -11,22 +11,33 @@ using std::accumulate;
 uint32_t computeMultiples(uint16_t max)
 {
 	int result(0);
-	for (int i = 1; i < 100; i++)
+	for (int i = 1; i < max; i++)
 	{
-		if (i % )
+	  if ((i % 3 == 0) || (i % 5 == 0))
+	  {
+	    result += i;
+	  }
 	}
 	
-	return accumulate(nums.begin(), nums.end(), 0);
+	return result;
 }
 
+uint32_t fastSum(uint32_t num, uint32_t max)
+{
+  return num *  ((max/num) * ((max/num) + 1) / 2);
+}
+
+uint32_t fastComputeMultiples(uint16_t max)
+{
+  return (fastSum(3, max) + fastSum(5, max)) - fastSum(15, max);
+}
 
 int main()
 {  
 	uint32_t multiples(computeMultiples(1000));
-
-	std::cout << "multiples of 5: " << multiples << std::endl;
-	std::cout << "multiples of 3: " << multiples << std::endl;
-	std::cout << "multiples of 3 and 5: " << (multiplesof3 + multiplesof5) << std::endl;
+	uint32_t fastmultiples(fastComputeMultiples(999));
+	std::cout << "multiples of 3 and 5: " << multiples << std::endl;
+	std::cout << "(fast) multiples of 3 and 5: " << fastmultiples << std::endl;
 
 	return 0;
 }
